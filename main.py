de1d6d5a4b0a197029becfca3affd4b146829fca
setografi <hitonaashbell@gmail.com>
2024-05-23 23:21:49 +0700

    update: flask setup update

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    # Optionally pass data to the template
-    title = "My Flask App"
-    message = "Hello World!"
-    
-    return render_template('index.html', title=title, message=message)
+from src import app
 
 if __name__ == '__main__':
     app.run(debug=True)
